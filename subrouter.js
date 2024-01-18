69b731cd890ce516bc9bdaaba6ca0c952b3a7300
YoHaiYo <12si47bun@naver.com>
2024-01-18 11:01:02 +0900

    노드서버폴더 내 리액트 프로젝트 생성

--- subrouter.js
+++ subrouter.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const normalrouter = express.Router();
 
-module.exports = normalrouter; // 이제 require로 받아서 쓸 수 있음. (리액트의 import)
+normalrouter.get("/", (req, res) => {
+  res.send("나는 누구일까요?")
+})
+normalrouter.get("/aaa", (req, res) => {
+  res.send("나는 aaa 누구일까요?")
+})
+
+module.exports = normalrouter;
--- web.js
+++ web.js
@@ -2,9 +2,10 @@
 const express = require("express");
 const path = require("path"); // 주소관련 메서드
 
+const normalrouter = require("./subrouter.js");
+
 const nodeserver = express();
 const port = process.env.PORT || 8006;
-
 
 // json보내기
 nodeserver.use(express.static(path.join(__dirname, ''))) // web.js와 같은 경로상 지정
@@ -19,7 +20,9 @@ nodeserver.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, 'build/index.html'))
 });
 
+nodeserver.use("/way", normalrouter);
 // 페이지 url 잘못됬을때 보여줄 페이지
+
 nodeserver.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, 'html/nopage.html'))
 })
